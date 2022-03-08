c06dd6c22115e20a132512b21ceaf9ac739063f6
Thang <thang15021999@gmail.com>
2022-03-08 09:52:30 +0700

    fix

--- generate_2.sh
+++ generate_2.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 python fairseq_cli/generate.py \
-    data-bin/data.tokenized.en-vi.v2 \
+    data-bin/data.tokenized.en-vi.v5 \
     --path checkpoints/envi_2/checkpoint_best.pt \
     --batch-size 32 \
     --beam 4 \
